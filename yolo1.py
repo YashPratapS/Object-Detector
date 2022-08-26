d8edf220c625b87269be6599a16c510847243d0b
YashPratapS <95158391+YashPratapS@users.noreply.github.com>
2022-08-26 20:44:00 +0530

    Update yolo1.py
    Added the feature of distance detector from the camera using a monocular camera. I also added the feature to capture the images of vehicles if they come into a particular range.

--- yolo1.py
+++ yolo1.py
@@ -5,6 +5,21 @@ import time
 threshold = 0.4
 supp_threshold = 0.3
 YOLO_IMAGE_SIZE = 320
+KNOWN_DISTANCE = 50 
+CAR_WIDTH = 1.76
+img_counter = 0
+
+def focal_length_finder (measured_distance, real_width, width_in_rf):
+    focal_length = (width_in_rf * measured_distance) / real_width
+
+    return focal_length
+
+# distance finder function 
+def distance_finder(focal_length, real_object_width, width_in_frmae):
+    distance = (real_object_width * focal_length) / width_in_frmae
+    return distance
+
+focal_car = focal_length_finder(KNOWN_DISTANCE, CAR_WIDTH, 2)
 
 def findObject(mobject):
     class_id =[]
@@ -27,7 +42,7 @@ def findObject(mobject):
     predicted_box_id = cv.dnn.NMSBoxes(box_location, conf_value, threshold, supp_threshold)
     return predicted_box_id, box_location, class_id, conf_value
 
-def show_detected_img(image, predicted_obj_id, bbox_location, class_ids, conf_values, width_ratio, height_ratio):
+def show_detected_img(image, predicted_obj_id, bbox_location, class_ids, conf_values, width_ratio, height_ratio, focal_car, CAR_WIDTH, img_counter):
     count =0 
     for index in predicted_obj_id:
         boundingbox = bbox_location[index]
@@ -38,34 +53,22 @@ def show_detected_img(image, predicted_obj_id, bbox_location, class_ids, conf_va
             conff = 'PERSON' + str(int(conf_values[index]*100)) + '%'
             cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255,0,0), 1)
         if class_ids[index]==2:
-            count = count+1
+            count = count+1 
+            distance = distance_finder(focal_car, CAR_WIDTH, w)
+            if distance < 0.6 and distance > 0.5:
+                crop_img = image[y-5:y + h+5, x-5:x + w+5]
+                img_name = "opencv_frame_{}.png".format(img_counter)
+                cv.imwrite(img_name, crop_img)
+                img_counter += 1  
+            conf = 'Distance:' + str(round(float(distance),2))
+            cv.putText(image, conf, (x+30,y+20), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (100,255,200), 1)
             cv.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
             conff = 'CAR' + str(int(conf_values[index]*100)) + '%'
             cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (0,0,255), 1)
-        if class_ids[index]==3:
-            count = count+1
-            cv.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
-            conff = 'MotorCycle' + str(int(conf_values[index]*100)) + '%'
-            cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255,255,255), 1)
-        if class_ids[index]==5:
-            count = count+1
-            cv.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
-            conff = 'BUS' + str(int(conf_values[index]*100)) + '%'
-            cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255,255,255), 1)
-        if class_ids[index]==9:
-            count = count+1
-            cv.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
-            conff = 'Traffic Light' + str(int(conf_values[index]*100)) + '%'
-            cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255,255,255), 1)
-        if class_ids[index]==12:
-            count = count+1
-            cv.rectangle(image, (x,y), (x+w,y+h), (255,0,0), 2)
-            conff = 'Stop Sign' + str(int(conf_values[index]*100)) + '%'
-            cv.putText(image, conff, (x,y-8), cv.FONT_HERSHEY_COMPLEX_SMALL, 0.5, (255,255,255), 1)
         
-    return count
+    return count, img_counter
 
-classes = ['car', 'person', 'motor cycle', 'stop sign', 'traffic light', 'bus']
+classes = ['car', 'person']
 
 model = cv.dnn.readNetFromDarknet('yolov3.cfg', 'yolov3.weights')
 model.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV)
@@ -88,7 +91,7 @@ while True:
     output = model.forward(output_layers)
     predicted_obj, box_location, class_id, conf_value = findObject(output)
 
-    count = show_detected_img(frame, predicted_obj, box_location, class_id, conf_value, org_width/YOLO_IMAGE_SIZE, org_height/YOLO_IMAGE_SIZE)
+    count, img_counter = show_detected_img(frame, predicted_obj, box_location, class_id, conf_value, org_width/YOLO_IMAGE_SIZE, org_height/YOLO_IMAGE_SIZE, focal_car, CAR_WIDTH, img_counter)
     counter = 'Number of Cars: ' + str(int(count))
     cv.putText(frame, counter, (10,20), cv.FONT_HERSHEY_COMPLEX_SMALL, 1, (255,0,0), 2)
     if count > 10:
